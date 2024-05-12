bf18a1e4f0ec67c886caa2976e1deedb3f4eac13
Petrov Petrovich <152518983+socute727@users.noreply.github.com>
2024-05-12 16:08:12 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
-
-linkDownload="https://github.com/socute727/pitch/"
-
 sudo rm -fv /usr/local/bin/pitch
-
-git clone $linkDownload
-#sudo mkdir /usr/local/bin/pitchsourcecode
+sudo mkdir /usr/local/bin/pitchsourcecode
 sudo mv pitch/pitch.py /usr/local/bin/pitchsourcecode
 sudo touch /usr/local/bin/pitch
 sudo echo "python /usr/local/bin/pitchsourcecode/pitch.py" >> /usr/local/bin/pitch
