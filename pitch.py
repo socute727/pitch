03022f65609bee38c65a8cca6d6e7f4a73098b87
Petrov Petrovich <152518983+socute727@users.noreply.github.com>
2024-05-14 22:47:04 +0500

    Add files via upload

--- pitch.py
+++ pitch.py
@@ -1,6 +1,9 @@
 import subprocess
 import os
 
+# Colors!
+
+White = "\033[37m"
 Cyan = "\033[36m"
 Purple = "\033[35m"
 Green = "\033[32m"
@@ -9,145 +12,118 @@ Yellow = "\033[33m"
 Blue = "\033[34m"
 Reset = "\033[0m"
 
+def run_command(command):
+    try:
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {command}")
+        print(f"Error message: {e}")
+        return None
+
 def get_username():
-    result = subprocess.run(['whoami'], capture_output=True, text=True)
-    username = result.stdout.strip()
-    return username
+    return run_command(['whoami'])
 
 def get_hostname():
-    result = subprocess.run(['uname', '-n'], capture_output=True, text=True)
-    hostname = result.stdout.strip()
-    return hostname
+    return run_command(['uname', '-n'])
 
 def get_distro_name():
-    result = subprocess.run(['lsb_release', '-d'], capture_output=True, text=True)
-    distro_full_info = result.stdout.strip()
-    distro_name = distro_full_info.split(':')[1].strip()
-    return distro_name
+    result = run_command(['lsb_release', '-d'])
+    if result:
+        return result.split(':')[1].strip()
+    else:
+        return 'Unknown or Red Hat-based distro'
 
 def get_window_manager():
-    wm = subprocess.run(['wmctrl', '-m'], capture_output=True, text=True)
-    wm_name = wm.stdout.split('\n')[0].split(':')[1].strip()
-    wlctrl_path = '/bin/wmctrl'
-    display_env = os.getenv('DISPLAY')
-    if display_env:
-        if wm_name:
-            return wm_name
-        elif os.path.exist(wlctrl_path):
-            return 'Unknown'
-        else:
-            return 'Try to install wmctrl'
-    else:
-        return 'Unknown'
+    result = run_command(['wmctrl', '-m'])
+    return result.split('\n')[0].split(':')[1].strip() if result else 'try to install/update wmctrl'
 
 def get_shell():
     shell_path = os.getenv('SHELL')
-    if shell_path:
-        return shell_path.split('/')[-1]
-    else:
-        return "Unknown"
+    return shell_path.split('/')[-1] if shell_path else "Unknown"
 
 def get_memory_info():
-    result = subprocess.run(['free', '-h'], stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8')
-    lines = output.split('\n')
-    for line in lines:
-        if 'Mem:' in line:
-            words = line.split()
-            memory_used = words[2]
-            return memory_used
+    result = run_command(['free', '-h'])
+    if result:
+        for line in result.split('\n'):
+            if 'Mem:' in line:
+                return line.split()[2]
+    return "Unknown"
 
 def get_total_memory():
-    result = subprocess.run(['free', '-h'], stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8')
-    lines = output.split('\n')
-    for line in lines:
-        if 'Mem:' in line:
-            words = line.split()
-            total_memory = words[1]
-            return total_memory
+    result = run_command(['free', '-h'])
+    if result:
+        for line in result.split('\n'):
+            if 'Mem:' in line:
+                return line.split()[1]
+    return "Unknown"
 
 def get_kernel_info():
-    result = subprocess.run(['uname', '-r'], capture_output=True, text=True)
-    kernel = result.stdout.strip()
-    return kernel
+    return run_command(['uname', '-r'])
 
-def get_init_system():
-    systemd_path = '/run/systemd/system'
-    runit_path = '/etc/runit'
-    openrc_path = '/usr/bin/openrc-init'
-    if subprocess.run(['systemctl'], capture_output=True, text=True).returncode == 0:
-        return 'systemd'
-    elif os.path.exists(runit_path):
+def get_openrc_system():
+    if os.path.exists('/bin/openrc'):
+        return 'openrc'
+    elif os.path.exists('/bin/runit'):
         return 'runit'
-    elif os.path.exists(openrc):
-        return 'openrc'   
+    elif os.path.exists('/bin/systemctl'):
+        return 'systemd'
+    elif os.path.exist('/bin/dinit'):
+        return 'dinit'
     else:
-        return 'unknown'
+        return 'Unknown'
 
-
-
-if "void" in get_distro_name().lower():
+if "artix" in get_distro_name().lower():
     def print_logo():
-        print(Green + " _    __      _     __")
-        print("| |  / /___  (_)___/ /")
-        print("| | / / __ \\/ / __  /")
-        print("| |/ / /_/ / / /_/ / ")
-        print("|___/\\____/_/\\__,_/" + Reset)
+        print(Cyan + "   ___       __  _      ")
+        print("  / _ | ____/ /_(_)_ __")
+        print(" / __ |/ __/ __/ /\\ \\ /")
+        print("/_/ |_/_/  \\__/_//_\\_\\ " + Reset)
         return ""
+
 elif "arch" in get_distro_name().lower():
     def print_logo():
-        print(Cyan + "    ___              __ ")
-        print("   /   |  __________/ /_")
-        print("  / /| | / ___/ ___/ __ \\")
-        print(" / ___ |/ /  / /__/ / / /")  
-        print("/_/  |_/_/   \\___/_/ /_/ " + Reset)
+        print(Cyan + "   ___           __ ")
+        print("  / _ | ________/ / ")
+        print(" / __ |/ __/ __/ _ \\")
+        print("/_/ |_/_/  \\__/_//_/" + Reset)
         return ""
+
+elif "void" in get_distro_name().lower():
+    def print_logo():
+        print(Green + " _   __     _    __")
+        print("| | / /__  (_)__/ /")
+        print("| |/ / _ \\/ / _  / ")
+        print("|___/\\___/_/\\_,_/  " + Reset)
+        return ""
+
 elif "gentoo" in get_distro_name().lower():
     def print_logo():
-        print(Cyan + "   ______           __            ")
-        print("  / ____/__  ____  / /_____  ____ ")
-        print(" / / __/ _ \\/ __ \\/ __/ __ \\/ __ \\")
-        print("/ /_/ /  __/ / / / /_/ /_/ / /_/ /")
-        print("\\____/\\___/_/ /_/\\__/\\____/\\____/ " + Reset)
+        print(Cyan + "  _____         __          ")
+        print(" / ___/__ ___  / /____  ___ ")
+        print("/ (_ / -_) _ \\/ __/ _ \\/ _ \\")
+        print("\\___/\\__/_//_/\\__/\\___/\\___/" + Reset)
         return ""
-elif "artix" in get_distro_name().lower():
-    def print_logo():
-        print(Cyan + "    ___         __  _     ")
-        print("   /   |  _____/ /_(_)  __")
-        print("  / /| | / ___/ __/ / |/_/")
-        print(" / ___ |/ /  / /_/ />  <  ")
-        print("/_/  |_/_/   \\__/_/_/|_|  " + Reset)
-        return ""
-elif "fefora" in get_distro_name().lower():
-    def print_logo():
-        print(Cyan + "    ____         __                ")
-        print("   / __/__  ____/ /___  _________ _")
-        print("  / /_/ _ \/ __  / __ \/ ___/ __ `/")
-        print(" / __/  __/ /_/ / /_/ / /  / /_/ /  ")
-        print("/_/  \___/\__,_/\____/_/   \__,_/   " + Reset)
-        return ""
+
 else:
     def print_logo():
-        print(Green + "    ____  _ __       __ ")
-        print("   / __ \\(_) /______/ /_ ")
-        print("  / /_/ / / __/ ___/ __ \\")
-        print(" / ____/ / /_/ /__/ / / /")
-        print("/_/   /_/\\__/\\___/_/ /_/ " + Reset)
+        print(Green + "   ___  _ __      __ ")
+        print("  / _ \\(_) /_____/ / ") 
+        print(" / ___/ / __/ __/ _ \\") 
+        print("/_/  /_/\\__/\\__/_//_/" + Reset)
         return ""
 
+# Here you can configurate wich information would display 
 
-
+print()
 print(print_logo())
-print("╭────────────────╮")
-print("│", Red + "" + Reset, "user:        │", Red + get_username() + Reset)
-print("│", Yellow + "" + Reset, "hname:       │", Yellow + get_hostname() + Reset)
-print("│", Green + "" + Reset, "distro:      │", Green + get_distro_name() + Reset)
-print("│", Purple + "󰌽" + Reset, "kernel:      │", Purple + get_kernel_info() + Reset)
-print("│ ", Cyan + "" + Reset, " WM:          │ ", Cyan + get_window_manager() + Reset, sep = "")
-print("│", Yellow + "" + Reset, "shell        │", Yellow + get_shell() + Reset)
-print("│", Purple + "󰜎" + Reset, "init system: │", Purple + get_init_system() + Reset)
-print("│", Cyan + "" + Reset, "memory:      │", Cyan + get_memory_info(), "|", get_total_memory() + Reset)
-print("├────────────────┤")
-print("│  colors:      │", Red + "  " + Yellow + "  " + Green + "  " + Purple + "  " + Cyan + "  " + Reset, " ")
-print("╰────────────────╯")
+print("╭─────      ───────╮")
+#print("User:", White + get_username() + Reset)
+#print("Hname:", White + get_hostname() + Reset)
+print("Distro:", White + get_distro_name() + Reset)
+print("Kernel:", White + get_kernel_info() + Reset)
+print("WM: ", White + get_window_manager() + Reset, sep = "")
+print("Shell:", White + get_shell() + Reset)
+print("Init system:", White + get_openrc_system() + Reset)
+print("╰─────      ───────╯")
+print()
