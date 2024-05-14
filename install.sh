b5651f793e82fbb659b514ac3b11e04ba4c5e2af
Petrov Petrovich <152518983+socute727@users.noreply.github.com>
2024-05-14 23:11:56 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo rm -fv /usr/local/bin/pitchsourcecode
 sudo rm -fv /usr/local/bin/pitch
 sudo mkdir /usr/local/bin/pitchsourcecode
-sudo mv pitch.py /usr/local/bin/pitchsourcecode
+sudo cp pitch.py /usr/local/bin/pitchsourcecode
 sudo touch /usr/local/bin/pitch
 sudo echo "python /usr/local/bin/pitchsourcecode/pitch.py" >> /usr/local/bin/pitch
 sudo chmod +x /usr/local/bin/pitch
